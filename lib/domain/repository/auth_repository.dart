32edeb5c19353b4180359a548c2cebcf954712d4
Mo7amd-Khalid <139893550+Mo7amd-Khalid@users.noreply.github.com>
2025-11-25 13:31:10 +0200

    make register api and make mapper file

--- lib/domain/repository/auth_repository.dart
+++ lib/domain/repository/auth_repository.dart
@@ -1,6 +1,7 @@
 import 'package:movies_app/data/models/login_request.dart';
 
 import '../../data/models/login_responce.dart';
+import '../entity/register_response_entity.dart';
 
 abstract interface class AuthRepository{
 
@@ -8,4 +9,12 @@ abstract interface class AuthRepository{
       LoginRequest loginRequest,
       );
 
+  Future<RegisterResponseEntity> register({
+    required String name,
+    required String email,
+    required String password,
+    required String confirmPassword,
+    required String phone,
+    required int avatarId,
+  });
 }
