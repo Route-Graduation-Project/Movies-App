41d3c3419195fecffcdf17ddda2eb06d20458427
Bassant Mohamed <bsntmhmd40@gmail.com>
2025-12-05 21:50:23 +0200

    update profile logic using API update profile

--- lib/update_data/api_manager.dart
+++ lib/update_data/api_manager.dart
@@ -3,25 +3,26 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import 'profile_response.dart';
 
-class ApiManager{
+class ApiManager {
   String url = "https://route-movie-apis.vercel.app/profile";
   late String token;
 
-  Future<void> getToken() async{
+  Future<void> _getToken() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     token = sharedPreferences.getString("token") ?? "";
   }
 
+  Future<void> _deleteToken() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.remove("token");
+  }
+
   Future<ProfileResponse> getProfile() async {
-    await getToken();
+    await _getToken();
     Dio dio = Dio();
     final response = await dio.get(
       url,
-      options: Options(
-        headers: {
-          "Authorization": "Bearer $token",
-        },
-      ),
+      options: Options(headers: {"Authorization": "Bearer $token"}),
     );
 
     ProfileResponse profileResponse = ProfileResponse.fromJson(response.data);
@@ -30,26 +31,31 @@ class ApiManager{
   }
 
   updateProfile({String? name, String? phone, int? avatarId}) async {
-    await getToken();
+    await _getToken();
     Dio dio = Dio();
     final body = {
       if (name != null) 'name': name,
       if (phone != null) 'phone': phone,
       if (avatarId != null) 'avaterId': avatarId,
     };
-    var response = await dio.patch(
+    dio.patch(
       url,
       data: body,
-      options: Options(
-        headers: {
-          "Authorization": "Bearer $token",
-        },
-      ),
+      options: Options(headers: {"Authorization": "Bearer $token"}),
     );
+  }
+
+  deleteProfile() async {
+    await _getToken();
+    Dio dio = Dio();
+    var response = await dio.delete(
+      url,
+      options: Options(headers: {"Authorization": "Bearer $token"}),
+    );
+    await _deleteToken();
 
     print(response.statusCode);
     print(response.statusMessage);
     print(response.data);
   }
 }
-
