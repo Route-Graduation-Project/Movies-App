259658e30de95dd03c9032a216f48dc4921f2354
shamiaamaher632-sys <shamiaamaher632@gmail.com>
2025-12-11 05:50:26 +0200

    add home tab screen and services

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import  'package:flutter/material.dart';
 
 import '../../data/models/movie_model.dart';
 import '../../data/data_source/movies_api_service.dart';
