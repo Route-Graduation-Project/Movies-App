a1e28ad002e1be1b84a679baec8c19598ab3efad
shamiaamaher632-sys <shamiaamaher632@gmail.com>
2025-12-13 04:41:01 +0200

    add home tab run-app

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,3 @@
-import 'package:flutter/material.dart';
-import 'presentation/home/home_screen.dart';
+import 'home_tab_all_in_one.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    );
-  }
+void main() => runApp(const MyApp());
